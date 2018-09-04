b29515003c069158e8047034e4a00f558fd6adc9
Aleks Tamarkin <aibolit@live.com>
2018-09-04 13:02:47 -0400

    changes

--- grails-app/conf/application.groovy
+++ grails-app/conf/application.groovy
@@ -1,10 +1,16 @@
 environments {
-  production {
-    dataSource {
-      def uri = new URI(System.env.CLEARDB_DATABASE_URL)
-      url = "jdbc:mysql://" + uri.host + ":" + uri.port + uri.path + "?" + uri.query
-      username = uri.userInfo.split(":")[0]
-      password = uri.userInfo.split(":")[1]
+    development {
+        dataSource {
+            println System.env
+        }
+    }   
+    production {
+        dataSource {
+            println System.env
+            def uri = new URI(System.env.CLEARDB_DATABASE_URL)
+            url = "jdbc:mysql://" + uri.host + ":" + uri.port + uri.path + "?" + uri.query
+            username = uri.userInfo.split(":")[0]
+            password = uri.userInfo.split(":")[1]
+        }
     }
-  }
 }
